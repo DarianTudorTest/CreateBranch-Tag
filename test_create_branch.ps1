15975380efecbf7b0266cfcee9a71349024ecbc2
Darian Tudor <Darian.Tudor@theaccessgroup.com>
2021-01-12 15:18:47 +0200

    Update Version.xml

--- test_create_branch.ps1
+++ test_create_branch.ps1
@@ -1,4 +1,4 @@
-param ($version)
+param ($version, $branchName) #(optional production name)
 git checkout master
 Write-Host "Loading Helper Functions" -ForegroundColor Cyan;
 $currentLocation = (Get-Item -Path ".\" -Verbose).FullName
@@ -9,20 +9,20 @@ $includePath = Join-Path $scriptPath -ChildPath "SharedConfigFunctions.ps1";
 $VersionsXml = [Xml](Get-Content $(Join-Path $scriptPath -ChildPath "Versions.xml") -ErrorVariable err)
 Write-Host "Getting versions" -ForegroundColor Cyan;
 $EVOVersion = GetVersionFromXML $VersionsXml "Nightly" "Evolutions";
-if($version -eq $null) {
-	$splitVers = GetVersionFromParam $EVOVersion
-	SetVersionFromParams $VersionsXml "Nightly" "Evolutions" $splitVers.Major ([int]$splitVers.Minor+1) $splitVers.BuildNumber $splitVers.Revision;
-	$VersionsXml.Save($(Join-Path $scriptPath -ChildPath "Versions.xml"));
-	$EVOVersion = GetVersionFromXML $VersionsXml "Nightly" "Evolutions";
-	git commit -a -m "Update Version.xml"
-	git tag $EVOVersion -a -m "Tag for version $EVOVersion"
-	git push --porcelain
-	git push --tags --porcelain
-	git checkout -b "release/$($splitVers.Major).$([int]$splitVers.Minor+1)"
-	git push origin "release/$($splitVers.Major).$([int]$splitVers.Minor+1)"
-}
-else
-{
+# if($version -eq $null) {
+	# $splitVers = GetVersionFromParam $EVOVersion
+	# SetVersionFromParams $VersionsXml "Nightly" "Evolutions" $splitVers.Major ([int]$splitVers.Minor+1) $splitVers.BuildNumber $splitVers.Revision;
+	# $VersionsXml.Save($(Join-Path $scriptPath -ChildPath "Versions.xml"));
+	# $EVOVersion = GetVersionFromXML $VersionsXml "Nightly" "Evolutions";
+	# git commit -a -m "Update Version.xml"
+	# git tag $EVOVersion -a -m "Tag for version $EVOVersion"
+	# git push --porcelain
+	# git push --tags --porcelain
+	# git checkout -b "releases/$($splitVers.Major).$([int]$splitVers.Minor+1)"
+	# git push origin "releases/$($splitVers.Major).$([int]$splitVers.Minor+1)"
+# }
+# else
+# {
 	$splitVers = GetVersionFromParam $version
 	SetVersionFromParams $VersionsXml "Nightly" "Evolutions" $splitVers.Major $splitVers.Minor $splitVers.BuildNumber $splitVers.Revision;
 	$VersionsXml.Save($(Join-Path $scriptPath -ChildPath "Versions.xml"));
@@ -31,7 +31,15 @@ else
 	git tag $EVOVersion -a -m "Tag for version $EVOVersion"
 	git push --porcelain
 	git push --tags --porcelain
-	git checkout -b "release/$($splitVers.Major).$($splitVers.Minor)"
-	git push origin "release/$($splitVers.Major).$($splitVers.Minor)"
-}
+	if($branchName -eq $null) 
+	{
+		git checkout -b "releases/$($splitVers.Major).$($splitVers.Minor)"
+		git push origin "releases/$($splitVers.Major).$($splitVers.Minor)"
+	}
+	else
+	{
+		git checkout -b "releases/$branchName"
+		git push origin "releases/$branchName"
+	}
+# }
 
