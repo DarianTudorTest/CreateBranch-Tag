69c9f0265a587f254131322f6e17435f17b6a0a1
Darian Tudor <Darian.Tudor@theaccessgroup.com>
2021-01-12 10:50:45 +0200

    incrementalBranch

--- test_create_branch.ps1
+++ test_create_branch.ps1
@@ -1,3 +1,9 @@
-$version = 2
-$version++
-git checkout -b branch$version
+$version = 2.5
+Write-Host "Loading Helper Functions" -ForegroundColor Cyan;
+$currentLocation = (Get-Item -Path ".\" -Verbose).FullName
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition;
+$includePath = Join-Path $scriptPath -ChildPath "SharedConfigFunctions.ps1";
+. "$includePath";
+$splitVers = GetVersionFromParam $version
+
+git checkout -b ([int]$splitVers.Minor+1)
